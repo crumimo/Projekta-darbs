acc74eaeb0ee0853aa14b963bef162cb63615188
avwavv <vavaveiko@gmail.com>
2025-03-08 02:11:24 +0200

    Animation states test

--- Assets/Script/Player/Movement.cs
+++ Assets/Script/Player/Movement.cs
@@ -6,11 +6,14 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     [SerializeField] private float speed;
+    private Animator animator;
     private Vector2 movement;
     private Rigidbody2D rb;
+   
 
     private void Start()
     {
+        animator = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
     }
 
@@ -18,10 +21,21 @@ public class Movement : MonoBehaviour
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+        
     }
 
     private void FixedUpdate()
     {
         rb.MovePosition(rb.position + movement.normalized * (speed * Time.fixedDeltaTime));
+        
+        if (movement != Vector2.zero)
+        {
+            animator.SetFloat("LastHorizontal", movement.x);
+            animator.SetFloat("LastVertical", movement.y);
+        }
     }
 }
