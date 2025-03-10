d32ff3f3d0f1fd00eb9f10929c9c0f16ee4feb91
avwavv <vavaveiko@gmail.com>
2025-03-10 19:25:02 +0200

    Hide and seek enemy
    made a simpler script for hide and seek enemy, using polygon collider instead of raycasts

---
+++ Assets/Script/Enemy/HideAndSeekEnemyWithCollider.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HideAndSeekEnemyRefactored : MonoBehaviour
+{
+    [SerializeField] private float GoDuration;
+    [SerializeField] private float StopDuration;
+    [SerializeField] private SpriteRenderer sprite;
+
+    private bool canMove;
+
+    private void Start()
+    {
+        StartCoroutine(CheckPlayer());
+    }
+
+    private IEnumerator CheckPlayer()
+    {
+        while (true)
+        {
+            if (canMove)
+            {
+                sprite.color = Color.green;
+                Debug.Log("Player can move");
+                yield return new WaitForSeconds(GoDuration);
+            }
+            else
+            {
+                sprite.color = Color.red;
+                Debug.Log("Hide!!!" );
+                yield return new WaitForSeconds(StopDuration);
+            }
+            canMove = !canMove;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!canMove)
+        {
+            if (other.CompareTag("Player"))
+            {
+                Debug.Log("Found ya!");
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+        }
+    }
+}
