6c83acbc405c148d6852a17ae49145db63e388e6
Linda Blomniece <blomniecelinda@gmail.com>
2025-03-30 12:21:19 +0300

    fix dialogue start

--- Assets/Script/Dialogue/ScriptsDialogue/DialogueTrigger.cs
+++ Assets/Script/Dialogue/ScriptsDialogue/DialogueTrigger.cs
@@ -9,23 +9,27 @@ public class DialogueTrigger : MonoBehaviour
     [SerializeField] private bool requiresQuietWhisper = false; 
     [SerializeField] private bool requiresEchoingRoots = false; 
     private bool canStartDialogue = false;
+    private bool playerInRange = false; // New flag to indicate if player is in range
 
     [SerializeField] private GameObject canTalk;
     [SerializeField] private GameObject cantTalk;
 
     [SerializeField] private float distanceToActivate;
 
-   
+    private void Update()
+    {
+        if (playerInRange && (canStartDialogue || requiresNoCombo) && Input.GetKeyDown(KeyCode.F))
+        {
+            TryStartDialogue();
+        }
+    }
 
     private void OnTriggerStay2D(Collider2D other)
     {
         if(other.CompareTag("Player"))
         {
-            if ((canStartDialogue || requiresNoCombo) && Input.GetKeyDown(KeyCode.F))
-            {
-                TryStartDialogue();
-            }
-            
+            playerInRange = true; // Set player in range flag
+
             if (canStartDialogue || requiresNoCombo)
             {
                 canTalk.SetActive(true);
@@ -39,12 +43,13 @@ public class DialogueTrigger : MonoBehaviour
             }
         }
     }
-    
 
     private void OnTriggerExit2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
+            playerInRange = false; // Clear player in range flag
+
             cantTalk.SetActive(false);
             canTalk.SetActive(false);
         }
