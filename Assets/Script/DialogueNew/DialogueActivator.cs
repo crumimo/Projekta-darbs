87045119c28dc8500ab01ceae0a3868b62b4a837
Linda Blomniece <blomniecelinda@gmail.com>
2025-03-31 18:16:20 +0300

    dialogue(demo)

---
+++ Assets/Script/DialogueNew/DialogueActivator.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class DialogueActivator : MonoBehaviour, IInteractable
+{
+    [SerializeField] private DialogueObject dialogueObject;
+
+    public void UpdateDialogueObject(DialogueObject dialogueObject)
+    {
+        this.dialogueObject = dialogueObject;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && other.TryGetComponent(out Movement player))
+        {
+            player.Interactable = this;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && other.TryGetComponent(out Movement player))
+        {
+            if (player.Interactable is DialogueActivator dialogueActivator && dialogueActivator == this)
+            {
+                player.Interactable = null;
+            }
+        }
+    }
+
+    public void Interact(Movement player)
+    {
+        foreach (DialogueResponseEvents responseEvents in GetComponents<DialogueResponseEvents>())
+        {
+            if (responseEvents.DialogueObject == dialogueObject)
+            {
+                player.DialogueUI.AddResponseEvents(responseEvents.Events);
+                break;
+            }
+        }
+
+        player.DialogueUI.ShowDialogue(dialogueObject);
+    }
+}
