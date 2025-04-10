9122d8227417ae2bb5af5a1fddfe8bc8756f048e
avwavv <vavaveiko@gmail.com>
2025-04-10 19:47:26 +0300

    hint panel for vekh house entrance

--- Assets/Script/Door.cs
+++ Assets/Script/Door.cs
@@ -1,24 +1,28 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Door : MonoBehaviour
 {
     private bool canEnter;
 
+    [SerializeField] private GameObject hintPanel;
+    [SerializeField] private TextMeshProUGUI text;
     [SerializeField] private Transform destination;
 
     private void OnTriggerStay2D(Collider2D other)
     {
         if (canEnter && Input.GetKeyDown(KeyCode.F))
         {
+            hintPanel.SetActive(false);
+            text.text = "";
             Movement playerPos = FindObjectOfType<Movement>();
             playerPos.transform.position = destination.position;
         }
         
         if (other.CompareTag("Player"))
         {
+            hintPanel.SetActive(true);
+            text.text = "Press F to enter";
             canEnter = true;
         }
     }
@@ -27,6 +31,8 @@ public class Door : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
+            hintPanel.SetActive(false);
+            text.text = "";
             canEnter = false;
         } 
     }
