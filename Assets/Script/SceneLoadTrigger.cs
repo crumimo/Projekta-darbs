4816c3575d167aa83b9f9e94e2605d2b619d728b
Linda Blomniece <blomniecelinda@gmail.com>
2025-05-14 10:59:38 +0300

    Load&UnloadScene

---
+++ Assets/Script/SceneLoadTrigger.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoadTrigger : MonoBehaviour
+{
+    [SerializeField] private string[] scenesToLoad; // Хранит имена сцен
+    [SerializeField] private string[] scenesToUnload;
+
+    private GameObject player;
+
+    private void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject == player)
+        {
+            LoadScenes();
+            UnloadScenes();
+        }
+    }
+
+    private void LoadScenes()
+    {
+        foreach (string sceneName in scenesToLoad)
+        {
+            bool isSceneLoaded = false;
+            for (int j = 0; j < SceneManager.sceneCount; j++)
+            {
+                Scene loadedScene = SceneManager.GetSceneAt(j);
+                if (loadedScene.name == sceneName)
+                {
+                    isSceneLoaded = true;
+                    break;
+                }
+            }
+
+            if (!isSceneLoaded)
+            {
+                SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Additive); // Передаём строку вместо SceneField
+            }
+        }
+    }
+
+    private void UnloadScenes()
+    {
+        foreach (string sceneName in scenesToUnload)
+        {
+            for (int j = 0; j < SceneManager.sceneCount; j++)
+            {
+                Scene loadedScene = SceneManager.GetSceneAt(j);
+                if (loadedScene.name == sceneName)
+                {
+                    SceneManager.UnloadSceneAsync(sceneName); // Передаём строку вместо SceneField
+                }
+            }
+        }
+    }
+}
