99a1eecd78950068ad9f5bf338665decb4aad521
Linda Blomniece <blomniecelinda@gmail.com>
2025-05-21 11:06:59 +0300

    fix dual obstacle

--- Assets/Script/Obstacles/Garden/DualObstacle.cs
+++ Assets/Script/Obstacles/Garden/DualObstacle.cs
@@ -1,58 +1,23 @@
 using UnityEngine;
-using System.Collections;
 
 public class DualObstacle : MonoBehaviour, IEffectable
 {
     [Header("Obstacle Parts")]
-    public GameObject activeObstacle;
-    public GameObject inactiveObstacle;
+    public GameObject activeObstacle; 
+    public GameObject inactiveObstacle; 
 
-    private bool isSwitched = false;
-
-    [Header("Fade Settings")]
-    public float fadeDuration = 0.5f;
+    private bool isSwitched = false; 
 
     [Header("Activation Settings")]
-    public float activationRadius = 5f;
+    public float activationRadius = 5f; 
 
     public int obstacleID;
-
     void Start()
     {
-        InitializeObstacles();
-    }
-    
-    private void InitializeObstacles()
-    {
-        if (activeObstacle != null) activeObstacle.SetActive(true);
-        if (inactiveObstacle != null) inactiveObstacle.SetActive(true);
-        
-        SetAlpha(activeObstacle, isSwitched ? 0f : 1f);
-        SetAlpha(inactiveObstacle, isSwitched ? 1f : 0f);
-        
-        if (isSwitched)
-        {
-            if (activeObstacle != null) activeObstacle.SetActive(false);
-        }
-        else
-        {
-            if (inactiveObstacle != null) inactiveObstacle.SetActive(false);
-        }
+        UpdateObstacleState();
     }
 
-    private void SetAlpha(GameObject obj, float alpha)
-    {
-        if (obj == null) return;
-        var renderer = obj.GetComponent<SpriteRenderer>();
-        if (renderer != null)
-        {
-            Color c = renderer.color;
-            c.a = alpha;
-            renderer.color = c;
-        }
-    }
-
-    private void UpdateObstacleStateImmediate()
+    private void UpdateObstacleState()
     {
         if (activeObstacle != null) activeObstacle.SetActive(!isSwitched);
         if (inactiveObstacle != null) inactiveObstacle.SetActive(isSwitched);
@@ -61,9 +26,16 @@ public class DualObstacle : MonoBehaviour, IEffectable
     public void ToggleObstacles()
     {
         isSwitched = !isSwitched;
-
+        UpdateObstacleState();
+        
         ObstacleStateManager.MarkObstacleSwitchedState(obstacleID, isSwitched);
         Debug.Log($"Obstacle state switched: {isSwitched}");
+    }
+    
+    public void ApplySwitchedState(bool isSwitched)
+    {
+        this.isSwitched = isSwitched;
+        UpdateObstacleState();
     }
 
     public bool ApplyEffect(EffectBase effect)
@@ -87,12 +59,6 @@ public class DualObstacle : MonoBehaviour, IEffectable
             }
         }
         return false;
-    }
-
-    public void ApplySwitchedState(bool isSwitched)
-    {
-        this.isSwitched = isSwitched;
-        UpdateObstacleStateImmediate();
     }
 
     public bool CanReceiveEffect(Vector3 playerPosition, float effectRadius, EffectBase effect)
